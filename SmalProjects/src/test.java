cba3bb9799e76553f77da9feb01fb845b183f2bd
cagrikucukbesleme <cagrikucukbesleme@gmail.com>
2021-10-14 11:41:52 +0300

    oop-uml diagrams

--- SmalProjects/src/test.java
+++ SmalProjects/src/test.java
@@ -1,17 +1,9 @@
 public class test {
 
     public static void main(String[] args) {
-        int t=0,i=0;
-        t = t + i;
-        i = i + 1;
-        System.out.println(t);
-        System.out.println(i);
-        int var1 = 5;
-        int var2 = 6;
-        if ((var2 = 1) == var1)
-            System.out.print(var2);
-        else
-            System.out.print(++var2);
+        int a = 4;
+
+        System.out.print((a >> 1)+(a << 2) );
         
 
     }
