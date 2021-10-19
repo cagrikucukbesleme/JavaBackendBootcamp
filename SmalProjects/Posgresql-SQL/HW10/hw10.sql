7b9def78587db5378b6e15cc7180da88b527d18a
cagrikucukbesleme <cagrikucukbesleme@gmail.com>
2021-10-19 23:57:09 +0300

    hw10

---
+++ SmalProjects/Posgresql-SQL/HW10/hw10.sql
@@ -0,0 +1,4 @@
+1-select  city,country from city left join country;
+2-select payment_id,first_name,last_name from customer right join payment;
+
+3-select rental_id,first_name,last_name from customer full join rental;
