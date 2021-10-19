0c4082528af984a51cc622f17a4523dd452d0e29
cagrikucukbesleme <cagrikucukbesleme@gmail.com>
2021-10-19 23:23:51 +0300

    hw9

---
+++ SmalProjects/Posgresql-SQL/HW9/hw9.sql
@@ -0,0 +1,7 @@
+1-select * from city inner join country on country.id=city.iD;
+2-select payment.payment_id,customer.first_name,customer.last_name from customer 
+inner join on payment.payment_id=customer.payment_id
+3-select rental.rental_id, customer.first_name,customer.last_name from rental inner join on
+rental.rental_id=customer.rental_id
+
+
